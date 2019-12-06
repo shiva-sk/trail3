bd96e6cd09f2abee3a099619a8a2a4cb5d1adf8b
shiva-sk <shivaysks@gmail.com>
2019-12-06 17:48:16 +0530

    hello

---
+++ src/org/emp/Employee.java
@@ -0,0 +1,37 @@
+package org.emp;
+
+public class Employee {
+	private void empId() {
+		// TODO Auto-generated method stub
+System.out.println("Emp Id is 1234");
+	}
+private void empName() {
+	// TODO Auto-generated method stub
+System.out.println("Emp name is siva");
+}
+private void empDOB() {
+	// TODO Auto-generated method stub
+System.out.println("Emp dob is 27-3-95");
+}
+private void empPhone() {
+	// TODO Auto-generated method stub
+System.out.println("Emp phone no is 123456789");
+}
+private void empemail() {
+	// TODO Auto-generated method stub
+System.out.println("email Id is saf@hsk.com");
+}
+private void empAddress() {
+	// TODO Auto-generated method stub
+System.out.println("Emp address is OMR");
+}
+public static void main(String[] args) {
+	Employee E=new Employee();
+	E.empId();
+	E.empName();
+	E.empAddress();
+	E.empDOB();
+	E.empPhone();
+	E.empemail();
+}
+}
